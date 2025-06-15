45801fd96cfac41c60818c36ff2bffa3df929431
Tiisetso Murray <timurray@student.hive.fi>
2025-06-15 15:26:14 +0300

    Spec.

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,25 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 12:44:43 by timurray          #+#    #+#             */
-/*   Updated: 2025/06/15 12:44:53 by timurray         ###   ########.fr       */
+/*   Updated: 2025/06/15 14:32:07 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+//Read one line from a file or stdin
+//If error or nothing or read, return NULL.
+//Return line should include \n EXCEPT when EOF is reached and has no \n
 char *get_next_line(int fd)
 {
+	char *line;
 
+
+	return (line);
 }
 
 int main(void)
 {
-	
+	printf("%i", BUFFER_SIZE);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,20 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 12:45:26 by timurray          #+#    #+#             */
-/*   Updated: 2025/06/15 12:46:22 by timurray         ###   ########.fr       */
+/*   Updated: 2025/06/15 14:47:18 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#include <stdio.h> //TODO: remove
+
+char *get_next_line(int fd);
 
 
 #endif
