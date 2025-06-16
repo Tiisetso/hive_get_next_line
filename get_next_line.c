ec143d801afd8e703a916fa4ad82b133b4d11b41
Tiisetso Murray <timurray@student.hive.fi>
2025-06-16 12:51:15 +0300

    Prelim exploration.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 12:44:43 by timurray          #+#    #+#             */
-/*   Updated: 2025/06/15 14:32:07 by timurray         ###   ########.fr       */
+/*   Updated: 2025/06/16 12:50:50 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,18 @@ char *get_next_line(int fd)
 	return (line);
 }
 
+
+#include <fcntl.h>
 int main(void)
 {
+	int fd;
+
+	fd = open("test.txt", O_RDONLY);
+	if(fd != -1)
+	{
+		
+	}
+	printf("\nfd = %i\n", fd);
 	printf("%i", BUFFER_SIZE);
 	return (1);
 }
